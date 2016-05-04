5fa7cc81e892bc84fcd0bcd990ed8fe6637ef68b
realincubus <kemnitz.stefan@googlemail.com>
2016-05-04 14:06:54 +0200

    changed to search the clang executable in this script

--- 1d/iterator/main.cpp
+++ 1d/iterator/main.cpp
@@ -9,12 +9,16 @@ int main(int argc, char** argv){
   vector<double> x(SIZE);
   vector<double> y(SIZE);
   
+  vector<double>::iterator b = x.begin();
+  vector<double>::iterator e = x.end();
+  int w = 1;
+  int z = 4;
 
-  for (vector<double>::iterator i = x.begin() ; 
-      i < x.end() ; 
+  for (vector<double>::iterator i = b ; 
+      i < e ; 
       i++ ){
       // to make it more pet friedly 
-      i[0] = 1.0;
+      //i[0] = 1.0;
   }
 
   return 0;
