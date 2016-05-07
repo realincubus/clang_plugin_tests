12a4f54aa099d35b234463c0c4be99726f323356
realincubus <kemnitz.stefan@googlemail.com>
2016-05-07 16:02:05 +0200

    missing ref file

---
+++ 1d/include/openacc/main.reference.cpp
@@ -0,0 +1,24 @@
+
+#include <vector>
+#include <list>
+#include <map>
+#include <iterator>
+#include <algorithm>
+
+#define SIZE 1000
+
+using namespace std;
+
+int main(int argc, char** argv){
+  double x[SIZE];
+  double y[SIZE];
+  
+
+  #pragma acc kernels 
+for (auto t1=0;t1<=999;++t1) {
+        x[t1] = y[t1]; 
+}
+
+
+  return 0;
+}
