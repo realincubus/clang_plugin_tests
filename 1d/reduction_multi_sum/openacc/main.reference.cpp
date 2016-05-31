1999d12d8ae1f7ff9ca0bd04fd00680becbbd89a
realincubus <kemnitz.stefan@googlemail.com>
2016-05-31 17:54:03 +0200

    added openacc reference files

---
+++ 1d/reduction_multi_sum/openacc/main.reference.cpp
@@ -0,0 +1,19 @@
+
+
+int main(int argc, char** argv){
+    
+  int sum_a = 0;
+  int sum_b = 0;  
+
+  int x[100];
+  int y[100];
+
+  #pragma acc kernels 
+for (auto t1=0;t1<=99;++t1) {
+        sum_a += x[t1];
+        sum_b += y[t1];
+}
+
+
+  return 0;
+}
