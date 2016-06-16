f556868482f5417c87e92e8e25e0ed6d2e11333d
realincubus <kemnitz.stefan@googlemail.com>
2016-06-16 14:50:08 +0200

    new failing test that shows that if a parameter is used twice everything crashes

---
+++ 2d/negative_innerloop_parameter/main.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+int main(int argc, char** argv){
+    
+  int x = 0;
+  int a = 0;
+
+  for (int i = 0; i < 100; ++i){
+      for (int j = 0; j < a; ++j){
+	x = 0;
+      }
+      for (int k = 0; k < a; ++k){
+	x = 0;
+      }
+  }
+
+  return 0;
+}
