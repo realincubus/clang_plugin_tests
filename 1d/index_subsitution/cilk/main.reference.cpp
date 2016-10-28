007568676e17785bdface719e938a673bf1a61fd
Stefan Kemnitz <kemnitz.stefan@googlemail.com>
2016-10-28 10:06:15 +0000

    adapted the index substitution test to reflect newest changes

--- 1d/index_subsitution/cilk/main.reference.cpp
+++ 1d/index_subsitution/cilk/main.reference.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cilk/cilk.h>
 
 
@@ -12,15 +11,17 @@ int main(int argc, char** argv){
   int i = 0;
 
   if ((NR >= 1) && (NZ >= 1)){
-  cilk_for (auto t1=0;t1<=NR-1;++t1) {
-    cilk_for (auto t2=0;t2<=std::min(NZ-1,t1-NR+2147483647);++t2) {
-      if ((t1 == NR-1) && (t2 >= 1)){
-        	Eps[(NR-1) * NZ +  t2] = -100.;
-      }      if (t2 == 0){
-        	Eps[t1 * NZ +  0] = -100.;
-      }    }
+  cilk_for (auto t1=0;t1<=NR-2;++t1) {
+    	Eps[t1 * NZ +  0] = -100.;
   }
-}
+  if (NZ >= 2){
+    	Eps[(NR-1) * NZ +  0] = -100.;
+    cilk_for (auto t2=1;t2<=NZ-1;++t2) {
+      	Eps[(NR-1) * NZ +  t2] = -100.;
+    }
+  }  if (NZ == 1){
+    	Eps[(NR-1) * NZ +  0] = -100.;
+  }}
 
   return 0;
 }
