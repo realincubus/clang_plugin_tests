dfb5102d119c6b6a2e56e55cee11e4ff8ff8502a
Stefan Kemnitz <kemnitz.stefan@googlemail.com>
2016-11-02 16:59:51 +0000

    new test to check whether a simple loop using iterators will generate the right implementation

--- 1d/iterator/cilk/main.reference.cpp
+++ 1d/iterator/cilk/main.reference.cpp
@@ -1,3 +1,4 @@
+#include <cilk/cilk.h>
 
 #include <vector>
 
@@ -8,11 +9,10 @@ using namespace std;
 int main(int argc, char** argv){
   vector<double> x(SIZE);
 
-  for (vector<double>::iterator i = x.begin() ; 
-      i != x.end() ; 
-      i++ ){
-	*i = 10;
-  }
+  cilk_for (auto t1=0;t1<=x.size()-1;++t1) {
+  	x[t1] = 10;
+}
+
 
   return 0;
 }
