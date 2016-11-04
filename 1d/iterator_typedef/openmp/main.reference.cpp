f7449c48b3dca1caed0f2106a72f9e32b2ada8ef
Stefan Kemnitz <kemnitz.stefan@googlemail.com>
2016-11-04 12:53:52 +0000

    new test to check whether iterators are understood if they are used with a typedef

---
+++ 1d/iterator_typedef/openmp/main.reference.cpp
@@ -0,0 +1,20 @@
+
+#include <vector>
+
+#define SIZE 1000
+
+using namespace std;
+
+int main(int argc, char** argv){
+  vector<double> x(SIZE);
+  typedef vector<double>::iterator my_iterator;
+
+  if (x.size() >= 1){
+  #pragma omp parallel for 
+  for (auto t1=0;t1<=x.size()-1;++t1) {
+    	x[t1] = 10;
+  }
+}
+
+  return 0;
+}
