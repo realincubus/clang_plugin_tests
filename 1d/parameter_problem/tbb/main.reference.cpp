ad0052c09609c93aca8a10c7e18344142d49acfb
Stefan Kemnitz <kemnitz.stefan@googlemail.com>
2017-11-27 13:05:34 +0000

    made reference impls for parameter problem

---
+++ 1d/parameter_problem/tbb/main.reference.cpp
@@ -0,0 +1,20 @@
+#include <tbb/parallel_for.h>
+
+
+int main(int argc, char** argv){
+  
+  int X = 10;
+  int C = 20;
+  int a[X*C];
+  int b[X*C];
+
+  if ((C >= 1) && (X >= 1)){
+  tbb::parallel_for (0,C-1 + 1,[&](int t1) {
+    for (auto t2=0; t2<=X-1;++t2) {
+            a[t1*X+t2] = b[t1*X+t2];
+    } 
+  } );
+}  
+
+  return 0;
+}
